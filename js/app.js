7d0dc154d932658239cac6e698c4e215c59b4eaa
Reid Miller <rmiller9@Reids-MacBook-Air.local>
2019-06-07 16:01:42 -0700

    Done

--- js/app.js
+++ js/app.js
@@ -5,6 +5,7 @@ var playerTurn = 1;
 var player = "X";
 var playerXValue = 4;
 var playerOValue = 1;
+var gameOver = false;
 var sapcesTaken = {
   c1: false,
   c2: false,
@@ -43,9 +44,12 @@ var score;
 var score;
 var playerTurnMessage;
 var gameTable;
+var img;
+var winnerMessage;
 
 // Event Listeners
 document.addEventListener('DOMContentLoaded', function(e) {
+  img = document.getElementsByTagName('img');
   cell1 = document.getElementById("cell1");
   cell2 = document.getElementById("cell2");
   cell3 = document.getElementById("cell3");
@@ -60,144 +64,130 @@ document.addEventListener('DOMContentLoaded', function(e) {
   score = document.getElementsByTagName("input")[1];
   playerTurnMessage = document.getElementById("turnmessage");
   gameTable = document.getElementById('gamebox');
+  winnerMessage = document.getElementById('winnermessage');
 
   gameTable.addEventListener('click', function(e) {
-    playerTurn ++;
-    whoseTurn();
-    if (e.target.id === "cell1" && playerTurn % 2 === 0) {
-      sapcesTaken[e.target.id] = null;
-      console.log(sapcesTaken[e.target.id]);
-      array1.push(playerXValue);
-      array4.push(playerXValue);
-      array7.push(playerXValue);
-      console.log(array1);
-      cell1.textContent = "X";
-    } else if (e.target.id === "cell2" && playerTurn % 2 === 0) {
-      sapcesTaken[e.target.id] = true;
-      array1.push(playerXValue);
-      array5.push(playerXValue);
-      console.log(array1);
-      cell2.textContent = "X";
-    } else if (e.target.id === "cell3" && playerTurn % 2 === 0) {
-      sapcesTaken[e.target.id] = true;
-      array1.push(playerXValue);
-      array6.push(playerXValue);
-      array8.push(playerXValue);
-      console.log(array1);
-      cell3.textContent = "X";
-    } else if (e.target.id === "cell4" && playerTurn % 2 === 0) {
-      sapcesTaken[e.target.id] = true;
-      array2.push(playerXValue);
-      array4.push(playerXValue);
-      console.log(array1);
-      cell4.textContent = "X";
-    } else if (e.target.id === "cell5" && playerTurn % 2 === 0) {
-      sapcesTaken[e.target.id] = true;
-      array2.push(playerXValue);
-      array5.push(playerXValue);
-      array7.push(playerXValue);
-      array8.push(playerXValue);
-      console.log(array1);
-      cell5.textContent = "X";
-    } else if (e.target.id === "cell6" && playerTurn % 2 === 0) {
-      sapcesTaken[e.target.id] = true;
-      array2.push(playerXValue);
-      array6.push(playerXValue);
-      console.log(array1);
-      cell6.textContent = "X";
-    } else if (e.target.id === "cell7" && playerTurn % 2 === 0) {
-      sapcesTaken[e.target.id] = true;
-      array3.push(playerXValue);
-      array4.push(playerXValue);
-      array8.push(playerXValue);
-      console.log(array1);
-      cell7.textContent = "X";
-    } else if (e.target.id === "cell8" && playerTurn % 2 === 0) {
-      sapcesTaken[e.target.id] = true;
-      array3.push(playerXValue);
-      array5.push(playerXValue);
-      console.log(array1);
-      cell8.textContent = "X";
-    } else if (e.target.id === "cell9" && playerTurn % 2 === 0) {
-      sapcesTaken[e.target.id] = true;
-      array3.push(playerXValue);
-      array6.push(playerXValue);
-      array7.push(playerXValue);
-      console.log(array1);
-      cell9.textContent = "X";
+    if (!gameOver) {
+      playerTurn ++;
+      whoseTurn();
+      if (e.target.id === "cell1" && playerTurn % 2 === 0) {
+        sapcesTaken[e.target.id] = null;
+        console.log(sapcesTaken[e.target.id]);
+        array1.push(playerXValue);
+        array4.push(playerXValue);
+        array7.push(playerXValue);
+        e.target.firstChild.src = "img/xzybit.png"
+      } else if (e.target.id === "cell2" && playerTurn % 2 === 0) {
+        sapcesTaken[e.target.id] = true;
+        array1.push(playerXValue);
+        array5.push(playerXValue);
+        e.target.firstChild.src = "img/xzybit.png"
+      } else if (e.target.id === "cell3" && playerTurn % 2 === 0) {
+        sapcesTaken[e.target.id] = true;
+        array1.push(playerXValue);
+        array6.push(playerXValue);
+        array8.push(playerXValue);
+        e.target.firstChild.src = "img/xzybit.png"
+      } else if (e.target.id === "cell4" && playerTurn % 2 === 0) {
+        sapcesTaken[e.target.id] = true;
+        array2.push(playerXValue);
+        array4.push(playerXValue);
+        e.target.firstChild.src = "img/xzybit.png"
+      } else if (e.target.id === "cell5" && playerTurn % 2 === 0) {
+        sapcesTaken[e.target.id] = true;
+        array2.push(playerXValue);
+        array5.push(playerXValue);
+        array7.push(playerXValue);
+        array8.push(playerXValue);
+        e.target.firstChild.src = "img/xzybit.png"
+      } else if (e.target.id === "cell6" && playerTurn % 2 === 0) {
+        sapcesTaken[e.target.id] = true;
+        array2.push(playerXValue);
+        array6.push(playerXValue);
+        e.target.firstChild.src = "img/xzybit.png"
+      } else if (e.target.id === "cell7" && playerTurn % 2 === 0) {
+        sapcesTaken[e.target.id] = true;
+        array3.push(playerXValue);
+        array4.push(playerXValue);
+        array8.push(playerXValue);
+        e.target.firstChild.src = "img/xzybit.png"
+      } else if (e.target.id === "cell8" && playerTurn % 2 === 0) {
+        sapcesTaken[e.target.id] = true;
+        array3.push(playerXValue);
+        array5.push(playerXValue);
+        e.target.firstChild.src = "img/xzybit.png"
+      } else if (e.target.id === "cell9" && playerTurn % 2 === 0) {
+        sapcesTaken[e.target.id] = true;
+        array3.push(playerXValue);
+        array6.push(playerXValue);
+        array7.push(playerXValue);
+        e.target.firstChild.src = "img/xzybit.png"
+      }
+  
+      if (e.target.id === "cell1" && playerTurn % 2 !== 0) {
+        sapcesTaken[e.target.id] = true;
+        array1.push(playerOValue);
+        array4.push(playerOValue);
+        array7.push(playerOValue);
+        e.target.firstChild.src = "img/omar.png"
+      } else if (e.target.id === "cell2" && playerTurn % 2 !== 0) {
+        sapcesTaken[e.target.id] = true;
+        array1.push(playerOValue);
+        array5.push(playerOValue);
+        e.target.firstChild.src = "img/omar.png"
+      } else if (e.target.id === "cell3" && playerTurn % 2 !== 0) {
+        sapcesTaken[e.target.id] = true;
+        array1.push(playerOValue);
+        array6.push(playerOValue);
+        array8.push(playerOValue);
+        e.target.firstChild.src = "img/omar.png"
+      } else if (e.target.id === "cell4" && playerTurn % 2 !== 0) {
+        sapcesTaken[e.target.id] = true;
+        array2.push(playerOValue);
+        array4.push(playerOValue);
+        e.target.firstChild.src = "img/omar.png"
+      } else if (e.target.id === "cell5" && playerTurn % 2 !== 0) {
+        sapcesTaken[e.target.id] = true;
+        array2.push(playerOValue);
+        array5.push(playerOValue);
+        array7.push(playerOValue);
+        array8.push(playerOValue);
+        e.target.firstChild.src = "img/omar.png"
+      } else if (e.target.id === "cell6" && playerTurn % 2 !== 0) {
+        sapcesTaken[e.target.id] = true;
+        array2.push(playerOValue);
+        array6.push(playerOValue);
+        e.target.firstChild.src = "img/omar.png"
+      } else if (e.target.id === "cell7" && playerTurn % 2 !== 0) {
+        sapcesTaken[e.target.id] = true;
+        array3.push(playerOValue);
+        array4.push(playerOValue);
+        array8.push(playerOValue);
+        e.target.firstChild.src = "img/omar.png"
+      } else if (e.target.id === "cell8" && playerTurn % 2 !== 0) {
+        sapcesTaken[e.target.id] = true;
+        array3.push(playerOValue);
+        array5.push(playerOValue);
+        e.target.firstChild.src = "img/omar.png"
+      } else if (e.target.id === "cell9" && playerTurn % 2 !== 0) {
+        sapcesTaken[e.target.id] = true;
+        array3.push(playerOValue);
+        array6.push(playerOValue);
+        array7.push(playerOValue);
+        e.target.firstChild.src = "img/omar.png"
+      }
+      ans1 = (array1.reduce((a,b) => a+b,0));
+      ans2 = (array2.reduce((a,b) => a+b,0));
+      ans3 = (array3.reduce((a,b) => a+b,0));
+      ans4 = (array4.reduce((a,b) => a+b,0));
+      ans5 = (array5.reduce((a,b) => a+b,0));
+      ans6 = (array6.reduce((a,b) => a+b,0));
+      ans7 = (array7.reduce((a,b) => a+b,0));
+      ans8 = (array8.reduce((a,b) => a+b,0));
+  
+      gameWin();
     }
 
-    if (e.target.id === "cell1" && playerTurn % 2 !== 0) {
-      sapcesTaken[e.target.id] = true;
-      array1.push(playerOValue);
-      array4.push(playerOValue);
-      array7.push(playerOValue);
-      console.log(array1);
-      cell1.textContent = "O";
-    } else if (e.target.id === "cell2" && playerTurn % 2 !== 0) {
-      sapcesTaken[e.target.id] = true;
-      array1.push(playerOValue);
-      array5.push(playerOValue);
-      console.log(array1);
-      cell2.textContent = "O";
-    } else if (e.target.id === "cell3" && playerTurn % 2 !== 0) {
-      sapcesTaken[e.target.id] = true;
-      array1.push(playerOValue);
-      array6.push(playerOValue);
-      array8.push(playerOValue);
-      console.log(array1);
-      cell3.textContent = "O";
-    } else if (e.target.id === "cell4" && playerTurn % 2 !== 0) {
-      sapcesTaken[e.target.id] = true;
-      array2.push(playerOValue);
-      array4.push(playerOValue);
-      console.log(array1);
-      cell4.textContent = "O";
-    } else if (e.target.id === "cell5" && playerTurn % 2 !== 0) {
-      sapcesTaken[e.target.id] = true;
-      array2.push(playerOValue);
-      array5.push(playerOValue);
-      array7.push(playerOValue);
-      array8.push(playerOValue);
-      console.log(array1);
-      cell5.textContent = "O";
-    } else if (e.target.id === "cell6" && playerTurn % 2 !== 0) {
-      sapcesTaken[e.target.id] = true;
-      array2.push(playerOValue);
-      array6.push(playerOValue);
-      console.log(array1);
-      cell6.textContent = "O";
-    } else if (e.target.id === "cell7" && playerTurn % 2 !== 0) {
-      sapcesTaken[e.target.id] = true;
-      array3.push(playerOValue);
-      array4.push(playerOValue);
-      array8.push(playerOValue);
-      console.log(array1);
-      cell7.textContent = "O";
-    } else if (e.target.id === "cell8" && playerTurn % 2 !== 0) {
-      sapcesTaken[e.target.id] = true;
-      array3.push(playerOValue);
-      array5.push(playerOValue);
-      console.log(array1);
-      cell8.textContent = "O";
-    } else if (e.target.id === "cell9" && playerTurn % 2 !== 0) {
-      sapcesTaken[e.target.id] = true;
-      array3.push(playerOValue);
-      array6.push(playerOValue);
-      array7.push(playerOValue);
-      console.log(array1);
-      cell9.textContent = "O";
-    }
-    ans1 = (array1.reduce((a,b) => a+b,0));
-    ans2 = (array2.reduce((a,b) => a+b,0));
-    ans3 = (array3.reduce((a,b) => a+b,0));
-    ans4 = (array4.reduce((a,b) => a+b,0));
-    ans5 = (array5.reduce((a,b) => a+b,0));
-    ans6 = (array6.reduce((a,b) => a+b,0));
-    ans7 = (array7.reduce((a,b) => a+b,0));
-    ans8 = (array8.reduce((a,b) => a+b,0));
-
-    gameWin();
   })
 
 })
@@ -206,48 +196,64 @@ function whoseTurn() {
     if(playerTurn % 2 === 0 || playerTurn === 0){
         player = 'X';
         //playerXValue = +4;
-        playerTurnMessage.textContent = "X goes Next";
+        playerTurnMessage.textContent = "Xzibit goes Next";
     } else if(!(playerTurn % 2 === 0)){
         player = 'O';
         //playerOValue ++;
-        playerTurnMessage.textContent = "O goes Next";
+        playerTurnMessage.textContent = "Omar goes Next";
     }
 }
 
 function gameWin() {
   if (ans1 === 12) {
-    console.log('X is the winner');
+    winnerMessage.textContent = 'Xzibit is the winner';
+    gameOver = true;
   } else if (ans2 === 12) {
-    console.log('X is the winner');
+     winnerMessage.textContent = 'Xzibit is the winner';
+    gameOver = true;
   } else if (ans3 === 12) {
-    console.log('X is the winner');
+     winnerMessage.textContent = 'Xzibit is the winner';
+    gameOver = true;
   } else if (ans4 === 12) {
-    console.log('X is the winner');
+     winnerMessage.textContent = 'Xzibit is the winner';
+    gameOver = true;
   } else if (ans5 === 12) {
-    console.log('X is the winner');
+     winnerMessage.textContent = 'Xzibit is the winner';
+    gameOver = true;
   } else if (ans6 === 12) {
-    console.log('X is the winner');
+     winnerMessage.textContent = 'Xzibit is the winner';
+    gameOver = true;
   } else if (ans7 === 12) {
-    console.log('X is the winner');
+     winnerMessage.textContent = 'Xzibit is the winner';
+    gameOver = true;
   } else if (ans8 === 12) {
-    console.log('X is the winner');
+     winnerMessage.textContent = 'Xzibit is the winner';
+    gameOver = true;
   }
 
   if (ans1 === 3) {
-    console.log('O is the winner');
+    winnerMessage.textContent = 'Omar is the winner';
+    gameOver = true;
   } else if (ans2 === 3) {
-    console.log('O is the winner');
+    winnerMessage.textContent = 'Omar is the winner';
+    gameOver = true;
   } else if (ans3 === 3) {
-    console.log('O is the winner');
+    winnerMessage.textContent = 'Omar is the winner';
+    gameOver = true;
   } else if (ans4 === 3) {
-    console.log('O is the winner');
+    winnerMessage.textContent = 'Omar is the winner';
+    gameOver = true;
   } else if (ans5 === 3) {
-    console.log('O is the winner');
+    winnerMessage.textContent = 'Omar is the winner';
+    gameOver = true;
   } else if (ans6 === 3) {
-    console.log('O is the winner');
+    winnerMessage.textContent = 'Omar is the winner';
+    gameOver = true;
   } else if (ans7 === 3) {
-    console.log('O is the winner');
+    winnerMessage.textContent = 'Omar is the winner';
+    gameOver = true;
   } else if (ans8 === 3) {
-    console.log('O is the winner');
+    winnerMessage.textContent = 'Omar is the winner';
+    gameOver = true;
   }
 }
